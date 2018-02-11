0810622aa78402d6d0b40dc009d07795d7137cfc
Tomasz <tkaluzny@outlook.com>
2018-02-11 17:43:31 +0000

    Added footer

--- ClientApp/components/App.vue
+++ ClientApp/components/App.vue
@@ -2,14 +2,17 @@
   <div>
     <toolbar></toolbar>
     <router-view></router-view>
+    <custom-footer></custom-footer>
   </div>
 </template>
 <script>
 import Toolbar from "./Toolbar.vue";
+import CustomFooter from "./CustomFooter.vue";
 
 export default {
   components: {
-    Toolbar
+    Toolbar,
+    CustomFooter
   }
 };
 </script>
---
+++ ClientApp/components/CustomFooter.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="footer">
+        <div class="hr"></div>
+        <div class="container">
+            <p>
+                <img src="/images/logo_grey.png" alt="logo" class="icon">
+                <span>
+                    <strong>Vuebnb</strong>. A full-stack Vue.js and Laravel demo app
+                </span>
+            </p>
+        </div>
+    </div>
+</template>
+<style lang="less" scoped>
+#footer {
+  margin-bottom: 3em;
+
+  .icon {
+    height: 23px;
+    display: inline-block;
+    margin-bottom: -6px;
+  }
+
+  p {
+    font-size: 15px;
+    color: #767676 !important;
+    display: flex;
+    width: 400px;
+    margin-left: auto;
+    margin-right: auto;
+
+    img {
+      padding-right: 6px;
+    }
+  }
+}
+
+.hr {
+  border-bottom: 1px solid #dbdbdb;
+  margin: 3em 0;
+}
+</style>
+
